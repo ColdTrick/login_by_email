89b615d2ef9e4eed0d32dfe920debf208ab3afb4
Jerôme Bakker <jeabakker@coldtrick.com>
2024-06-06 15:01:45 +0200

    chore: added Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file was created by Translation Editor v13.1.1
+ * On 2024-06-06 15:01
+ */
+
+return array (
+  'login_by_email:menu:login:new_request' => 'Nieuwe aanmeldcode aanvragen',
+  'login_by_email:settings:code_validity' => 'Aanmeldcode geldigheid timeout',
+  'login_by_email:settings:code_validity:help' => 'Hoe lang (in minuten) is de aanmeldcode/-link geldig',
+  'login_by_email:code:title' => 'Aanmeldcode invoeren',
+  'login_by_email:code:description' => 'Er is een e-mail naar je verzonden, controleer je inbox voor een aanmeldcode.
+Je kunt de link in de e-mail gebruiken om je direct aan te melden, of je geeft de code uit de e-mail hieronder op.
+
+PS: als je geen e-mail vindt in je inbox, controleer de spam map.',
+  'login_by_email:code:code' => 'Geef hier de aanmeldcode op',
+  'login_by_email:notification:request_link:subject' => 'Nieuw aanmeldverzoek voor %s',
+  'login_by_email:notification:request_link:message' => 'Je hebt een aanmeldverzoek aangevraagd voor %s.
+
+Je kunt je direct aanmelden met de volgende link:
+%s
+
+of je kunt onderstaande code invoeren op de aanmeldpagina:
+%s',
+  'login_by_email:action:request_link:success' => 'Aanmeldcode aangevraagd, controleer je e-mail inbox',
+  'login_by_email:action:code:error:code' => 'Ongeldige aanmeldcode. Probeer het nogmaals.',
+  'login_by_email:action:code:error:code_expired' => 'De aanmeldcode is verlopen. Vraag een nieuwe code aan.',
+);
