320eda8b4b35f50921ea9660ccda7ceda9252183
Jerôme Bakker <jeabakker@coldtrick.com>
2026-01-21 09:36:39 +0100

    chore: wrapping up v2.0

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -9,7 +9,7 @@ use Elgg\Router\Middleware\SignedRequestGatekeeper;
 
 return [
 	'plugin' => [
-		'version' => '1.0',
+		'version' => '2.0',
 	],
 	'bootstrap' => Bootstrap::class,
 	'settings' => [
